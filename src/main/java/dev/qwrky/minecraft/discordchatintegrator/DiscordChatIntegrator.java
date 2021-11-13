64c6794e57c8d5c5532f2c3a3a404ba9dfc80314
Ash Johnston <oj.nologic@gmail.com>
2021-11-13 22:33:07 +0000

    Initial Commit

---
+++ src/main/java/dev/qwrky/minecraft/discordchatintegrator/DiscordChatIntegrator.java
@@ -0,0 +1,17 @@
+package dev.qwrky.minecraft.discordchatintegrator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiscordChatIntegrator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
